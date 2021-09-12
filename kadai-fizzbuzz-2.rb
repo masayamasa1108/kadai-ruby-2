b0b29dc8f9fea2d6fb479d035c5d57143fbc28fc
masayamasa1108 <masayamasa1108@gmail.com>
2021-09-12 14:22:05 +0000

    create fizzbuzz-2 ruby file

---
+++ kadai-fizzbuzz-2.rb
@@ -0,0 +1,25 @@
+# fizzbuzz method
+def fizzbuzz(num)
+  int_3 = 3
+  int_5 = 5
+  int_15 = int_3 * int_5
+
+  if num % int_15 == 0
+    "FizzBuzz"
+  elsif num % int_5 == 0
+    "Buzz"
+  elsif num % int_3 == 0
+    "Fizz"
+  else
+    num
+  end
+end
+
+
+# process start
+num_max = 100
+
+(1..num_max).each do |num|
+  puts fizzbuzz(num)
+end
+
